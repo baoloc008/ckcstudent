15a21c90a4ac81283ced446892a6504e1623a177
baoloc008 <baoloc008@gmail.com>
2020-04-11 12:07:06 +0700

    update UI HomePage

---
+++ lib/pages/app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class AppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(56.0);
+
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+class LeftAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "CKC STUDENTS",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "Phòng CTCT - HSSV",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RightAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Tuần 25",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          Text(
+            "Năm học: 2019 - 2020",
+            style: TextStyle(
+              fontSize: 9,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TopBanner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+      padding: EdgeInsets.fromLTRB(28, 20, 28, 20),
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(41, 50, 132, 229),
+              offset: Offset(0, 5),
+              blurRadius: 16,
+              spreadRadius: 0,
+            )
+          ]),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "THÔNG BÁO SINH VIÊN",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 14),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset("images/iconNotification.png"),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.check_circle,
+                          size: 12,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          "Bổ sung hồ sơ HSSV",
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.check_circle,
+                          size: 12,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          "Nhận xét giảng viên",
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.check_circle,
+                          size: 12,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          "Miễn giảm kinh phí ",
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.check_circle,
+                          size: 12,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          "Các thông báo khác",
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Text(
+              "Sinh viên phải thường xuyên cập nhật các thông báo mới trên ứng dụng và website...",
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TopBanner(),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: FlexibleSpaceBar(
+          centerTitle: false,
+          titlePadding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 10),
+          title: Container(
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  LeftAppBar(), // left content
+                  RightAppBar(), // right content
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: HomePageBody(),
+    );
+  }
+}
