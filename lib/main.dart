641cc9b73b5a491f4043a602e64031ba829db3e8
baoloc008 <baoloc008@gmail.com>
2020-04-11 12:56:15 +0700

    set android status bar transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:ckcstudent/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(CKCStudentApp());
 
 class CKCStudentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
     return MaterialApp(
       title: "CKC STUDENTS",
       theme: ThemeData(primaryColor: Colors.blue),
