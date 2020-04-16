b3cf3e0dd5d66830e51c30f822be1018f0527c87
baoloc008 <baoloc008@gmail.com>
2020-04-16 22:23:18 +0700

    test get data from firebase

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:ckcstudent/pages/widgets/image_carousel.dart';
 import 'package:ckcstudent/pages/widgets/category_list/category_list.dart';
 import 'package:ckcstudent/pages/widgets/top_banner/top_banner.dart';
 import 'package:ckcstudent/service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ckcstudent/pages/widgets/right_appbar.dart';
 
@@ -23,37 +24,50 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('appconfig').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return LinearProgressIndicator();
+        }
+        print(snapshot.data.documents[0].data['category_list']);
+        return Scaffold(
+          appBar: AppBar(
+            titleSpacing: 30,
+            centerTitle: false,
+            title: Text(
+              'CKC STUDENTS',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'SFCompactDisplay-Bold',
+              ),
+            ),
+            actions: <Widget>[
+              Container(
+                padding: EdgeInsets.only(right: 30, bottom: 10),
+                child: RightAppBar(),
+              )
+            ],
+          ),
+          body: ListView(
+            children: <Widget>[
+              TopBanner(),
+              SizedBox(
+                height: 10,
+              ),
+              // CategoryHeader(),
+              CategoryList(),
+              ImageCarousel(),
+            ],
+          ),
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 30,
-        centerTitle: false,
-        title: Text(
-          'CKC STUDENTS',
-          style: TextStyle(
-            fontSize: 20,
-            fontFamily: 'SFCompactDisplay-Bold',
-          ),
-        ),
-        actions: <Widget>[
-          Container(
-            padding: EdgeInsets.only(right: 30, bottom: 10),
-            child: RightAppBar(),
-          )
-        ],
-      ),
-      body: ListView(
-        children: <Widget>[
-          TopBanner(),
-          SizedBox(
-            height: 10,
-          ),
-          // CategoryHeader(),
-          CategoryList(),
-          ImageCarousel(),
-        ],
-      ),
-    );
+    return _buildBody(context);
   }
 }
