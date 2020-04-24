a9d8589512ab1c64efaf5da6aac3acd6fa719e8c
baoloc008 <baoloc008@gmail.com>
2020-04-25 00:24:41 +0700

    add flutter webview plugin

--- lib/home_page/widgets/category_list/category_item.dart
+++ lib/home_page/widgets/category_list/category_item.dart
@@ -1,44 +1,66 @@
 import 'package:ckcstudent/models/app_config.dart';
 import 'package:ckcstudent/widgets/general/box_shadow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class CategoryItem extends StatelessWidget {
   final CategoryModel categoryModel;
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
 
   CategoryItem({this.categoryModel});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 135,
-      height: 135,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [BlueBoxShadow],
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: 50,
-            child: FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: categoryModel.icon,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WebviewScaffold(
+            url: categoryModel.url,
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                'CKC STUDENTS',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'SFCompactDisplay-Bold',
+                ),
+              ),
             ),
           ),
-          SizedBox(height: 15),
-          Text(
-            categoryModel.text,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'SFCompactDisplay-Semibold',
-              fontSize: 12,
+        ),
+      ),
+      child: Container(
+        width: 135,
+        height: 135,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BlueBoxShadow],
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 50,
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: categoryModel.icon,
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 15),
+            Text(
+              categoryModel.text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'SFCompactDisplay-Semibold',
+                fontSize: 12,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
