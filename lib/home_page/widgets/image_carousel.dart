a41831eee26f8b9a3e5791562a7f0b0202e9db11
baoloc008 <baoloc008@gmail.com>
2020-05-03 17:35:22 +0700

    update homepage carousel

--- lib/home_page/widgets/image_carousel.dart
+++ lib/home_page/widgets/image_carousel.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class CarouselWithIndicator extends StatefulWidget {
   final List<String> imgList;
@@ -14,89 +14,61 @@ class CarouselWithIndicator extends StatefulWidget {
 }
 
 class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
-  List<Widget> imageSliders() {
-    return widget.imgList
-        .map((item) => Container(
-              child: Container(
-                margin: EdgeInsets.all(5.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    child: Stack(
-                      children: <Widget>[
-                        Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                        Positioned(
-                          bottom: 0.0,
-                          left: 0.0,
-                          right: 0.0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color.fromARGB(200, 0, 0, 0),
-                                  Color.fromARGB(0, 0, 0, 0)
-                                ],
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter,
-                              ),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 20.0),
-                            child: Text(
-                              'No. ${widget.imgList.indexOf(item)} image',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
-            ))
-        .toList();
-  }
-
   int _current = 0;
 
   @override
   Widget build(BuildContext context) {
-    print(widget.imgList);
-    return Scaffold(
-      appBar: AppBar(title: Text('Carousel with indicator demo')),
-      body: Column(children: [
-        CarouselSlider(
-          items: this.imageSliders(),
-          options: CarouselOptions(
-              autoPlay: true,
-              enlargeCenterPage: true,
-              aspectRatio: 2.0,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _current = index;
-                });
-              }),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: widget.imgList.map((url) {
-            int index = widget.imgList.indexOf(url);
-            return Container(
-              width: 8.0,
-              height: 8.0,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _current == index
-                    ? Color.fromRGBO(0, 0, 0, 0.9)
-                    : Color.fromRGBO(0, 0, 0, 0.4),
-              ),
-            );
-          }).toList(),
-        ),
-      ]),
-    );
+    return Column(children: [
+      CarouselSlider(
+        items: widget.imgList
+            .map((item) => Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      height: 200,
+                      child: CachedNetworkImage(
+                        fit: BoxFit.fill,
+                        imageUrl: item,
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) => Center(
+                          child: CircularProgressIndicator(
+                              value: downloadProgress.progress),
+                        ),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                    ),
+                  ),
+                ))
+            .toList(),
+        options: CarouselOptions(
+            autoPlay: true,
+            enlargeCenterPage: true,
+            height: 200,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            }),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: widget.imgList.map((url) {
+          int index = widget.imgList.indexOf(url);
+          return Container(
+            width: 8.0,
+            height: 8.0,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _current == index
+                  ? Theme.of(context).primaryColor
+                  : Color.fromRGBO(0, 0, 0, 0.1),
+            ),
+          );
+        }).toList(),
+      ),
+    ]);
   }
 }
 
@@ -108,31 +80,12 @@ class ImageCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
       margin: EdgeInsets.only(
         top: 9, // 30 - blurRadius - offset.y
         bottom: 38,
       ),
-      child: CarouselSlider(
-        options: CarouselOptions(
-          autoPlay: true,
-          initialPage: 0,
-        ),
-        items: imgList
-            .map((item) => Container(
-                  margin: EdgeInsets.only(left: 10, right: 10),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: item,
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ))
-            .toList(),
+      child: CarouselWithIndicator(
+        imgList: this.imgList,
       ),
     );
   }
